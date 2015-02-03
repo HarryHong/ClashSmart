0d68f090f12b4ce1c8bcf67bf014629662ab2726
Heyz008 <ShuaJJ@Jiaqis-iMac.local>
2015-02-02 19:47:17 -0500

    Initial Commit

---
+++ Clash Smart/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  Clash Smart
+//
+//  Created by Joshua Jiang on 2015-02-02.
+//  Copyright (c) 2015 Joshua Jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
